dd520a1ef44310a57e110c90e1f251a19a60c016
suresh <suresh.avlm@gmail.com>
2021-05-01 22:16:48 +0530

    initial check-ins

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express'); //download express library from npm
-
 const chalk = require('chalk'); //print message in color code
+const fs = require('fs');
+var products = {};
+var isUserLoggedIn = false;
+
+var PORT = process.env.PORT || 8080;
 
 const app = express();
 
-/* starting server might take few sec **/
-app.listen(3000, function() {
-	console.log(chalk.green('Server started listening on port 3000'));
+fs.readFile('./data/products.json', function(err, data) {
+	products = JSON.parse(data.toString());
 });
+
+/* starting server might take few sec **/
+app.listen(PORT, function() {
+	console.log(chalk.green('Server started listening on port', PORT));
+});
+
+
+//middleware or tunnel
+app.use(function(req, res, next) {
+	console.log('*** Request received *** :', new Date());
+	next(); //pass request to next handler
+	//I want to check whether user is logged in or not
+	//I want to validate a token beofre access any data
+	//I want to print request received date for every handler
+});
+
+function validateUser(req, res, next) {
+	if(isUserLoggedIn){
+		next(); //control passed to next handler
+	}
+	else {
+		res.sendFile(__dirname + '/login.html'); //sending 
+	}
+}
 
 /* http://localhost:3000/ */
 app.get('/', function(req, res) {
@@ -16,12 +43,16 @@ app.get('/', function(req, res) {
 
 /* http://localhost:3000/login */
 app.post('/login', function(req, res) {
-	res.send('<h3>Login Success</h3>');
+	isUserLoggedIn = true;
+	res.sendFile(__dirname + '/home.html');
 });
 
 /* http://localhost:3000/products */
-app.get('/products', function(req, res) {
-	return products;
+app.get('/invoice', validateUser, function(req, res) {
+	res.download(__dirname + '/data/invoice.pdf');
 });
 
+app.get('/products', function(req, res) {
+	res.json(products);
+});
 console.log('**** Program ended ****');
